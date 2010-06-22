ab3fe294c60b611ddeacf4e6c0d092b0b35e276d
dziegler <david.ziegler@gmail.com>
2010-06-21 17:52:27 -0700

    patch setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ class _M2CryptoBuildExt(build_ext.build_ext):
         
         self.swig_opts = ['-I%s' % i for i in self.include_dirs + \
                           [opensslIncludeDir]]
+        self.swig_opts.append('-I/usr/include/openssl')
         self.swig_opts.append('-includeall')
         #self.swig_opts.append('-D__i386__') # Uncomment for early OpenSSL 0.9.7 versions, or on Fedora Core if build fails
         #self.swig_opts.append('-DOPENSSL_NO_EC') # Try uncommenting if you can't build with EC disabled
